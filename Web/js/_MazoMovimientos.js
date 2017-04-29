2d6c0d8860818b5ec32cf7d92d7dc621fa89a491
lopezasin <ivan@lopez.es>
2017-04-29 14:27:36 +0200

    yiu

--- Web/js/_MazoMovimientos.js
+++ Web/js/_MazoMovimientos.js
@@ -2233,18 +2233,33 @@ GLFVIKHXMD=function(){
 
 
 function checkConnection() {
-    var networkState = navigator.connection.type;
 
-    var states = {};
-    states[Connection.UNKNOWN]  = 'Unknown connection';
-    states[Connection.ETHERNET] = 'Ethernet connection';
-    states[Connection.WIFI]     = 'WiFi connection';
-    states[Connection.CELL_2G]  = 'Cell 2G connection';
-    states[Connection.CELL_3G]  = 'Cell 3G connection';
-    states[Connection.CELL_4G]  = 'Cell 4G connection';
-    states[Connection.CELL]     = 'Cell generic connection';
-    states[Connection.NONE]     = 'No network connection';
+	var isCordovaApp = document.URL.indexOf('http://') === -1
+		&& document.URL.indexOf('https://') === -1;
 
-    alert('Connection type: ' + states[networkState]);
-	return 
+
+	if (isCordovaApp==true)
+	{
+		
+		var networkState = navigator.connection.type;
+		var states = {};
+    
+
+
+		states[Connection.UNKNOWN]  = 'Unknown connection';
+		states[Connection.ETHERNET] = 'Ethernet connection';
+		states[Connection.WIFI]     = 'WiFi connection';
+		states[Connection.CELL_2G]  = 'Cell 2G connection';
+		states[Connection.CELL_3G]  = 'Cell 3G connection';
+		states[Connection.CELL_4G]  = 'Cell 4G connection';
+		states[Connection.CELL]     = 'Cell generic connection';
+		states[Connection.NONE]     = 'No network connection';
+
+		SVHMCOYULR('Connection type: ' + states[networkState]);
+	}
+	else
+	{
+		SVHMCOYULR('No es Cordova');
+	}
+	
 }
