060a27218f39864df9d4b3be2a4da820b77dfad2
lopezasin <ivan@lopez.es>
2018-12-23 18:31:16 +0100

    assqqq

--- Web/js/_Level.js
+++ Web/js/_Level.js
@@ -4274,7 +4274,7 @@ RSHJSKMBRP=function(Tipo)
 								function(response) {
 									socket.emit("Consola","EO SOLO ");
 									//socket.emit("Consola","EO ID "+response.id);
-									socket.emit("Consola","EO "+response)
+									socket.emit("Consola",response)
 									//socket.emit("Consola","EO "+response.id + " | " + response.name + " | " + response.email + " | " +response.picture.url);
 									MCFSLYOMHE();
 									HDGGWGGSVH=true;
